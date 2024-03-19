aaf144aa6f379f01e10a6a61a5adbaa3f880a386
nwokolo emmanuel <99542337+nwokoloemmanuel6@users.noreply.github.com>
2024-03-20 00:23:51 +0100

    Update variables.tf

--- kubernetes/nginx-controller/variables.tf
+++ kubernetes/nginx-controller/variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
   description = "This is the domain name"
   type        = string
-  default     = "nwokolo.live"
+  default     = "berthaweb.com"
 }
   
 variable "alt_domain_name" {
   description = "This is the alternative domain name"
   type        = string
-  default     = "*.nwokolo.live"
+  default     = "*.berthaweb.com"
 }
